7756b73c1a3097a2002a8eb4f18dbd3c2b807edf
Katerina <katerina3113@bk.ru>
2023-03-01 00:37:43 +0300

    Домашнее задание

--- homework/Program.cs
+++ homework/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] ARR;
+Console.WriteLine("Введите значения через пробел:");
+string array = Console.ReadLine();
+ARR = array.Split(' ');
+var result = new string[ARR.Length];
+var count = 0;
+foreach (var l in ARR)
+{
+    if(l.Length<4)
+    {
+        result[count] = l;
+        count++;
+    }
+}
+Console.Write(string.Join(Environment.NewLine, result));
